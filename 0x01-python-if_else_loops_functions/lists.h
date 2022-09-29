07317b42aa3021884fb325b13edd2999ff5c964b
FRESH <nyeiamara1@gmail.com>
2022-09-29 12:45:59 -0700

    My first task

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -2,10 +2,12 @@
 #define LISTS_H
 
 #include <stdlib.h>
+
 /**
  * struct listint_s - single linked list
  * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  */
 typedef struct listint_s
