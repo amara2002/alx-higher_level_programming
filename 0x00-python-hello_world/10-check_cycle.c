0934316c8217397fe738501b95eca91dac80fe1b
FRESH <nyeiamara1@gmail.com>
2022-09-29 10:39:18 -0700

    10th task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,18 +6,20 @@
  *
  * Return: 1 if the list has a cycle, 0 if it does't
  */
-int check_cycle(listint_t *list)
-{
- listint_t *slow = list;
- listint_t *fast = list;
-if (!list)
-         return (0);
-while (slow && fast && fast ->next)
+ int check_cycle(listint_t *list)
  {
-         slow = slow->next;
-          fast = fast->next->next;
-          if (slow == fast)
+       listint_t *slow = list;
+       listint_t *fast = list;
+
+       if (!list)
+             return (0);
+
+       while (slow && fast && fast->next)
+       {
+              slow = slow->next;
+              fast = fast->next->next;
+              if (slow == fast)
                         return (1);
- }
-return (0);
+       }
+         return (0);
 }
